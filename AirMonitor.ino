f94e30e0bff437ba6b466c249ba7d5204e9402c8
Joseph Wu <kaysoky88@gmail.com>
2015-05-12 13:19:24 -0700

    Cleaned up air monitor code.  Normalized comments/spacing and added comments where appropriate.  Surrounded all Serial printing with #ifdef DEBUG blocks.
    Removed unused BLE sample code.

--- AirMonitor.ino
+++ AirMonitor.ino
@@ -20,7 +20,7 @@
 #define GATT_HANDLE_C_RX_DATA       17  // 0x11, supports "write" operation
 #define GATT_HANDLE_C_TX_DATA       20  // 0x14, supports "read" and "indicate" operations
 
-// uncomment the following line for debug serial output
+// Toggle for debug serial output
 #define DEBUG
 
 // BLE state/link status tracker
@@ -38,219 +38,238 @@ SoftwareSerial bleSerialPort(3, 4);
 BGLib ble112((HardwareSerial *)&bleSerialPort, 0, 1);
 #define BGAPI_GET_RESPONSE(v, dType) dType *v = (dType *)ble112.getLastRXPayload()
 
+// Sensor data
 unsigned int pm;
 unsigned int voc;
 unsigned int temp;
 unsigned int humd;
+
+// Bluetooth attributes
 uint8 bluetooth_data[6] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+
+// State of Bluetooth connection
 boolean bluetoothConnected;
 
-//pin change ISR in interrupt vector table -> already defined in software serial
-//ISR(PCINT0_vect)  {
-//
-//}
-
-//watchdog timer ISR definition in interrupt vector table
+// Watchdog timer ISR definition in interrupt vector table
 ISR(WDT_vect)  {
-  wdt_disable();  //disable watchdog timer
+  wdt_disable();  // Disable watchdog timer
 }
 
 void setup() {
-  resetWatchdogTimer ();  // do this first in case WDT fires
-  I2c.begin(); // Opens & joins the irc bus as master
-  delay(100); // Waits to make sure everything is powered up before sending or receiving data  
-  I2c.timeOut(500); // Sets a timeout to ensure no locking up of sketch if I2C communication fails
-  
-  pinMode(VOC_MEASURE_ENABLE_PIN, OUTPUT);
-  digitalWrite(VOC_MEASURE_ENABLE_PIN, LOW);
-  
-  pinMode(PM_LED_PIN, OUTPUT);
-  digitalWrite(PM_LED_PIN, HIGH);
-  
-  pinMode(MEASURE_WAKEUP_PIN, INPUT_PULLUP);
-  pciSetup(8);
-  
-  Serial.begin(38400);
-  while (!Serial);
-  bluetoothSetup();
+    resetWatchdogTimer ();  // do this first in case WDT fires
+    I2c.begin(); // Opens & joins the irc bus as master
+    delay(100); // Waits to make sure everything is powered up before sending or receiving data
+    I2c.timeOut(500); // Sets a timeout to ensure no locking up of sketch if I2C communication fails
+
+    // Enable VOC sensor
+    pinMode(VOC_MEASURE_ENABLE_PIN, OUTPUT);
+    digitalWrite(VOC_MEASURE_ENABLE_PIN, LOW);
+
+    // Enable PM sensor
+    pinMode(PM_LED_PIN, OUTPUT);
+    digitalWrite(PM_LED_PIN, HIGH);
+
+    // Enable button-based wakeup
+    pinMode(MEASURE_WAKEUP_PIN, INPUT_PULLUP);
+    pciSetup(8);
+
+    #ifdef DEBUG
+        Serial.begin(38400);
+        while (!Serial);
+    #endif
+    bluetoothSetup();
 }
 
-//sets up pin change ISR for the specified pin number
+// Sets up pin change ISR for the specified pin number
 void pciSetup(byte pin)  {
     *digitalPinToPCMSK(pin) |= bit (digitalPinToPCMSKbit(pin));  // enable pin
-    PCIFR  |= bit (digitalPinToPCICRbit(pin)); // clear any outstanding interrupt
-    PCICR  |= bit (digitalPinToPCICRbit(pin)); // enable interrupt for the group
+    PCIFR |= bit (digitalPinToPCICRbit(pin)); // clear any outstanding interrupt
+    PCICR |= bit (digitalPinToPCICRbit(pin)); // enable interrupt for the group
 }
 
 void loop() {
-  if (digitalRead(MEASURE_WAKEUP_PIN)) {
-    Serial.print("Entering sleep...");
-    delay(100);
-    enterSleep();
-    Serial.println("woke up!");
-  }
-  // keep polling for new data from BLE
-  ble112.checkActivity();  
+    if (digitalRead(MEASURE_WAKEUP_PIN)) {
+        #ifdef DEBUG
+            Serial.print("Entering sleep...");
+        #endif
+        
+        delay(100);
+        enterSleep();
+        
+        #ifdef DEBUG
+            Serial.println("Woke up!");
+        #endif
+    }
+    
+    // Keep polling for new data from BLE
+    ble112.checkActivity();
 
-  printTempHumd();  
-  printVOC();
-  printPM();
-  
-  Serial.println(temp, BIN);
-  Serial.println(humd, BIN);
-  Serial.println(voc, BIN);
-  Serial.println(pm, BIN);
-  
-  Serial.println();
-  formatBluetoothData();
+    // Acquire data from sensors
+    printTempHumd();
+    printVOC();
+    printPM();
 
-  uint8_t result = ble112.ble_cmd_attributes_write(GATT_HANDLE_C_TX_DATA,0, 6, (const uint8*)bluetooth_data);
-  
-  delay(500);
+    #ifdef DEBUG
+        Serial.println(temp, BIN);
+        Serial.println(humd, BIN);
+        Serial.println(voc, BIN);
+        Serial.println(pm, BIN);
+        Serial.println();
+    #endif
+    
+    formatBluetoothData();
+
+    uint8_t result = ble112.ble_cmd_attributes_write(GATT_HANDLE_C_TX_DATA, 
+        0, 6, (const uint8*) bluetooth_data);
+
+    delay(500);
 }
 
+/**
+ * Stuffs the sensor data into 6-bytes:
+ * 14-bits Temperature
+ * 14-bits Humidity
+ * 10-bits VOC
+ * 10-bits PM
+ */
 void formatBluetoothData()  {
-  bluetooth_data[5] = pm & 0xFF;
-  bluetooth_data[4] = (pm >> 8) & B00000011;
-  
-  bluetooth_data[4] = bluetooth_data[4] + ((voc << 2) & 0xFF);
-  bluetooth_data[3] = ((voc >> 6) & B0001111);
-  
-  bluetooth_data[3] = bluetooth_data[3] + ((humd << 4) & 0xFF);
-  bluetooth_data[2] = (humd >> 4) & 0xFF;
-  bluetooth_data[1] = (humd >> 12) & B00000011;
-  
-  bluetooth_data[1] = bluetooth_data[1] + ((temp << 2) & 0xFF);
-  bluetooth_data[0] = (temp >> 6) & 0xFF;
+    bluetooth_data[5] = pm & 0xFF;
+    bluetooth_data[4] = (pm >> 8) & B00000011;
+
+    bluetooth_data[4] |= ((voc << 2) & 0xFF);
+    bluetooth_data[3] = ((voc >> 6) & B0001111);
+
+    bluetooth_data[3] |= ((humd << 4) & 0xFF);
+    bluetooth_data[2] = (humd >> 4) & 0xFF;
+    bluetooth_data[1] = (humd >> 12) & B00000011;
+
+    bluetooth_data[1] |= ((temp << 2) & 0xFF);
+    bluetooth_data[0] = (temp >> 6) & 0xFF;
 }
 
 void printPM()  {
-  getPM();
-  
-  Serial.print("PM:   ");
-  delay(3);
-  Serial.print(pm / 10.23);
-  delay(2);
-  Serial.println("%");
-  delay(1);
+    getPM();
+
+    #ifdef DEBUG
+        Serial.print("PM:   ");
+        delay(3);
+        Serial.print(pm / 10.23);
+        delay(2);
+        Serial.println("%");
+        delay(1);
+    #endif
 }
 
 void printTempHumd()  {
-  getTempHumd();
-    
-  Serial.print("TEMP: ");
-  delay(3);
-  Serial.print(temp / (float)0x4000 * 165 - 40);
-  delay(2);
-  Serial.println(" C");
-  delay(1);
-  
-  Serial.print("HUMD: ");
-  delay(3);
-  Serial.print(humd / (float)0x4000 * 100);
-  delay(2);
-  Serial.println("%");
-  delay(1);
+    getTempHumd();
+
+    #ifdef DEBUG
+        Serial.print("TEMP: ");
+        delay(3);
+        Serial.print(temp / (float)0x4000 * 165 - 40);
+        delay(2);
+        Serial.println(" C");
+        delay(1);
+
+        Serial.print("HUMD: ");
+        delay(3);
+        Serial.print(humd / (float)0x4000 * 100);
+        delay(2);
+        Serial.println("%");
+        delay(1);
+    #endif
 }
 
 void printVOC()  {
-  getVOC();
-  
-  Serial.print("VOC:  ");
-  delay(3);
-  Serial.print(voc / 10.23);
-  delay(2);
-  Serial.println("%");
-  delay(1);
+    getVOC();
+
+    #ifdef DEBUG
+        Serial.print("VOC:  ");
+        delay(3);
+        Serial.print(voc / 10.23);
+        delay(2);
+        Serial.println("%");
+        delay(1);
+    #endif
 }
 
-void getPM()  {
-  digitalWrite(PM_LED_PIN, LOW);
-  delayMicroseconds(280);
-  pm = analogRead(3);
-  delayMicroseconds(40);
-  digitalWrite(PM_LED_PIN, HIGH);
-  delayMicroseconds(9680);
+void getPM() {
+    digitalWrite(PM_LED_PIN, LOW);
+    delayMicroseconds(280);
+    
+    pm = analogRead(3);
+    delayMicroseconds(40);
+    
+    digitalWrite(PM_LED_PIN, HIGH);
+    delayMicroseconds(9680);
 }
 
 void getVOC()  {
-  digitalWrite(VOC_MEASURE_ENABLE_PIN, HIGH);
-  delay(50);
-  voc = analogRead(1);
-  digitalWrite(VOC_MEASURE_ENABLE_PIN, LOW);
+    digitalWrite(VOC_MEASURE_ENABLE_PIN, HIGH);
+    delay(50);
+    
+    voc = analogRead(1);
+    digitalWrite(VOC_MEASURE_ENABLE_PIN, LOW);
 }
 
 int getTempHumd()  {
-  uint8_t nackack = 100; // Setup variable to hold ACK/NACK resopnses  
+    byte data[4];
+    uint8_t nackack = 100; // Setup variable to hold ACK/NACK resopnses
 
-  //Measurement Request
-//  while (nackack != 0)  {
+    // Measurement request
     nackack = I2c.write(TEMP_HUMD_ADDRESS, 0x01);
-//    delay(I2C_POLLING_DELAY); // Wait to prevent overpolling
-//    Serial.println(nackack);
-//  } 
-  delay(55);  //Acquisition Time
-  nackack = 100; // Setup variable to hold ACK/NACK resopnses  
-  
-  //Data Fetch
-//  while (nackack != 0)  {
+    delay(55); // Acquisition Time
+    nackack = 100; // Setup variable to hold ACK/NACK resopnses
+
+    // Data fetch
     nackack = I2c.read(TEMP_HUMD_ADDRESS, 4);
-//    delay(I2C_POLLING_DELAY);
-//  }
-  
-  byte data[4];
-  
-  for (int i = 3; i >= 0; i--)  {
-    data[i] = I2c.receive();
-  }
+    for (int i = 3; i >= 0; i--)  {
+        data[i] = I2c.receive();
+    }
 
-  humd = ((data[3] & B00111111) << 8) + data[2];
-  temp = (data[1] << 6) + (data[0] >> 2);
-
-  //Serial.println(data[3] >> 6, BIN);  //prints read status bit
+    humd = ((data[3] & B00111111) << 8) + data[2];
+    temp = (data[1] << 6) + (data[0] >> 2);
 }
 
 void resetWatchdogTimer()  {
-  // clear various "reset" flags
-  MCUSR = 0;     
-  // allow changes, disable reset, clear existing interrupt
-  WDTCSR = bit (WDCE) | bit (WDE) | bit (WDIF);
-  // set interrupt mode and an interval (WDE must be changed from 1 to 0 here)
-  WDTCSR = bit (WDIE) | bit (WDP3) | bit (WDP0);    // set WDIE, and 8 seconds delay
-  wdt_reset();  
+    // clear various "reset" flags
+    MCUSR = 0;
+    
+    // allow changes, disable reset, clear existing interrupt
+    WDTCSR = bit (WDCE) | bit (WDE) | bit (WDIF);
+    
+    // set interrupt mode and an interval (WDE must be changed from 1 to 0 here)
+    WDTCSR = bit (WDIE) | bit (WDP3) | bit (WDP0); // set WDIE, and 8 seconds delay
+    wdt_reset();
 }
 
 void enterSleep()  {
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN);   // sleep mode is set here
-  
-  byte old_ADCSRA = ADCSRA; // disable ADC to save power 
-  ADCSRA = 0;               // disable ADC to save power
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);   // sleep mode is set here
 
-  power_all_disable();      //power off ADC, Timer 0 and 1, serial interface
-  noInterrupts();           //timed sequence coming up
-  resetWatchdogTimer();     //get watchdog timer ready
-  sleep_enable();           //enables the sleep bit in the mcucr register so sleep is possible. just a safety pin
-  interrupts();             //interrupts are required now
-  sleep_cpu ();             //PROGRAM SLEEPS HERE! 
-  sleep_disable();          // cancel sleep as a precaution
-  power_all_enable ();      // power everything back on
-  ADCSRA = old_ADCSRA;      // re-enable ADC conversion
+    byte old_ADCSRA = ADCSRA; // disable ADC to save power
+    ADCSRA = 0;               // disable ADC to save power
+
+    power_all_disable();      // power off ADC, Timer 0 and 1, serial interface
+    noInterrupts();           // timed sequence coming up
+    resetWatchdogTimer();     // get watchdog timer ready
+    sleep_enable();           // enables the sleep bit in the mcucr register so sleep is possible. just a safety pin
+    interrupts();             // interrupts are required now
+    sleep_cpu();              // PROGRAM SLEEPS HERE!
+    sleep_disable();          // cancel sleep as a precaution
+    power_all_enable();       // power everything back on
+    ADCSRA = old_ADCSRA;      // re-enable ADC conversion
 }
 
 
-/****************************BLUETOOTH STUFF*************************************/
+/******************************************************************************/
+/*                                 BLUETOOTH                                  */
+/******************************************************************************/
 
-// bluetooth initialization sequence
 void bluetoothSetup() {
-     // set up internal status handlers (these are technically optional)
+    // set up internal status handlers (these are technically optional)
     ble112.onBusy = onBusy;
     ble112.onIdle = onIdle;
     ble112.onTimeout = onTimeout;
-
-    // ONLY enable these if you are using the <wakeup_pin> parameter in your firmware's hardware.xml file
-    // BLE module must be woken up before sending any UART data
-    //ble112.onBeforeTXCommand = onBeforeTXCommand;
-    //ble112.onTXCommandComplete = onTXCommandComplete;
 
     // set up BGLib event handlers
     ble112.ble_evt_system_boot = my_ble_evt_system_boot;
@@ -261,7 +280,7 @@ void bluetoothSetup() {
     // open BLE software serial port
     bleSerialPort.begin(38400);
 
-    my_ble_evt_system_boot( NULL);
+    my_ble_evt_system_boot(NULL);
 }
 
 // ================================================================
@@ -271,13 +290,13 @@ void bluetoothSetup() {
 // called when the module begins sending a command
 void onBusy() {
     // turn LED on when we're busy
-    //digitalWrite(LED_PIN, HIGH);
+    // digitalWrite(LED_PIN, HIGH);
 }
 
 // called when the module receives a complete response or "system_boot" event
 void onIdle() {
     // turn LED off when we're no longer busy
-    //digitalWrite(LED_PIN, LOW);
+    // digitalWrite(LED_PIN, LOW);
 }
 
 // called when the parser does not read the expected response in the specified time limit
@@ -288,7 +307,7 @@ void onTimeout() {
 // called immediately before beginning UART TX of a command
 void onBeforeTXCommand() {
     // wake module up (assuming here that digital pin 5 is connected to the BLE wake-up pin)
-   
+
     // give a bit of a gap between parsing the wake-up event and allowing the command to go out
     delayMicroseconds(1000);
 }
@@ -296,10 +315,8 @@ void onBeforeTXCommand() {
 // called immediately after finishing UART TX
 void onTXCommandComplete() {
     // allow module to return to sleep (assuming here that digital pin 5 is connected to the BLE wake-up pin)
-//    digitalWrite(BLE_WAKEUP_PIN, LOW);
+    // digitalWrite(BLE_WAKEUP_PIN, LOW);
 }
-
-
 
 // ================================================================
 // APPLICATION EVENT HANDLER FUNCTIONS
@@ -319,7 +336,7 @@ void my_ble_evt_system_boot(const ble_msg_system_boot_evt_t *msg) {
     #endif
 
     // system boot means module is in standby state
-    //ble_state = BLE_STATE_STANDBY;
+    // ble_state = BLE_STATE_STANDBY;
     // ^^^ skip above since we're going right back into advertising below
 
     // set advertisement interval to 200-300ms, use all advertisement channels
@@ -330,8 +347,8 @@ void my_ble_evt_system_boot(const ble_msg_system_boot_evt_t *msg) {
     // USE THE FOLLOWING TO LET THE BLE STACK HANDLE YOUR ADVERTISEMENT PACKETS
     // ========================================================================
     // start advertising general discoverable / undirected connectable
-   // ble112.ble_cmd_gap_set_mode(BGLIB_GAP_GENERAL_DISCOVERABLE, BGLIB_GAP_UNDIRECTED_CONNECTABLE);
-    //while (ble112.checkActivity(1000));
+    // ble112.ble_cmd_gap_set_mode(BGLIB_GAP_GENERAL_DISCOVERABLE, BGLIB_GAP_UNDIRECTED_CONNECTABLE);
+    // while (ble112.checkActivity(1000));
 
     // USE THE FOLLOWING TO HANDLE YOUR OWN CUSTOM ADVERTISEMENT PACKETS
     // =================================================================
@@ -423,12 +440,12 @@ void my_ble_evt_connection_status(const ble_msg_connection_status_evt_t *msg) {
             ble_state = BLE_STATE_CONNECTED_MASTER;
         }
     }
-    
+
     bluetoothConnected = (msg -> flags & 0x01) == 0x01;
 
     // update "encrypted" status
     ble_encrypted = msg -> flags & 0x02;
-    
+
     // update "bonded" status
     ble_bonding = msg -> bonding;
 }
@@ -442,12 +459,12 @@ void my_ble_evt_connection_disconnect(const struct ble_msg_connection_disconnect
     #endif
 
     // set state to DISCONNECTED
-    //ble_state = BLE_STATE_DISCONNECTED;
+    // ble_state = BLE_STATE_DISCONNECTED;
     // ^^^ skip above since we're going right back into advertising below
 
     // after disconnection, resume advertising as discoverable/connectable
-    //ble112.ble_cmd_gap_set_mode(BGLIB_GAP_GENERAL_DISCOVERABLE, BGLIB_GAP_UNDIRECTED_CONNECTABLE);
-    //while (ble112.checkActivity(1000));
+    // ble112.ble_cmd_gap_set_mode(BGLIB_GAP_GENERAL_DISCOVERABLE, BGLIB_GAP_UNDIRECTED_CONNECTABLE);
+    // while (ble112.checkActivity(1000));
 
     // after disconnection, resume advertising as discoverable/connectable (with user-defined advertisement data)
     ble112.ble_cmd_gap_set_mode(BGLIB_GAP_USER_DATA, BGLIB_GAP_UNDIRECTED_CONNECTABLE);
