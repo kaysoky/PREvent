7198d2acb736166e26adb54a67768b06a8e890d6
Joseph Wu <kaysoky88@gmail.com>
2015-05-23 15:24:11 -0700

    Add most of the resources from the amberMZ/atomSPHERE project, minus the layouts, menu, and strings. Add WelcomeActivity, but link to the device scan for now.

---
+++ src/com/prevent/WelcomeActivity.java
@@ -0,0 +1,23 @@
+package com.prevent;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Shows a static welcome page
+ */
+public class WelcomeActivity extends Activity {
+
+    public void onLoginClick(View view) {
+        Intent goToLoginPage = new Intent(this, DeviceScanActivity.class);
+        startActivity(goToLoginPage);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome);
+    }
+}
