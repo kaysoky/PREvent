cedd79647e1dc2f00dfad3c2c7db451e2f8c307e
Joseph Wu <kaysoky88@gmail.com>
2015-05-28 16:54:09 -0700

    Temporarily breaking all graphing functionality. Improved Map UI button based interaction.  Buttons added to filter data over time.

--- README.md
+++ README.md
@@ -5,6 +5,10 @@ Built with [Django](https://docs.djangoproject.com/en/1.8/)
 * [Django Registration](https://django-registration.readthedocs.org/en/latest/)
 * [Django REST Framework](http://www.django-rest-framework.org/)
 * [Django Filter](https://github.com/alex/django-filter)
+* [Bootstrap](http://getbootstrap.com/)
+* [Keen IO Dashboard](https://github.com/keen/dashboards)
+* [Google Heatmaps](https://developers.google.com/maps/documentation/javascript/heatmaplayer)
+* [Rickshaw](https://github.com/shutterstock/rickshaw)
 
 ## Installation
 ```
--- static/map-ui-main.js
+++ static/map-ui-main.js
@@ -1,19 +1,59 @@
+var map;
+var tempHeatmap;
+var humiHeatmap;
+var partHeatmap;
+var vocsHeatmap;
+var tempData = new google.maps.MVCArray();
+var humiData = new google.maps.MVCArray();
+var partData = new google.maps.MVCArray();
+var vocsData = new google.maps.MVCArray();
+
 function loadUI() {
-    FetchData();
+    // Initialize the data arrays and map layers
+    tempHeatmap = new google.maps.visualization.HeatmapLayer({
+        data: tempData
+    });
+    humiHeatmap = new google.maps.visualization.HeatmapLayer({
+        data: humiData
+    });
+    partHeatmap = new google.maps.visualization.HeatmapLayer({
+        data: partData
+    });
+    vocsHeatmap = new google.maps.visualization.HeatmapLayer({
+        data: vocsData
+    });
+    
+    FetchData('', InitializeHeatmap);
 }
 
-var aggregateData = {};
-var tempData = [];
-var humiData = [];
-var partData = [];
-var vocsData = [];
+function InitializeHeatmap(center) {
+    var mapOptions = {
+        zoom: 15, // Level at which some landmarks and streets are visible
+        center: center,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        maxIntensity: 100
+    };
+
+    // Initialize Google Maps layer
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    
+    // Default to PM display
+    togglePartMap();
+}
+
+// Semaphore to prevent concurrency messiness
+var isWorking = false;
 
 /**
  * Grabs data and places it in some script-wide variables
  */
-function FetchData() {
-    $.get('/data/').done(function(data) {
-        InitializeHeatmap(ParseData(data));
+function FetchData(query, callback) {
+    if (isWorking) return;
+    
+    isWorking = true;
+    $.get('/data/?' + query).done(function(data) {
+        callback(ParseData(data));
+        isWorking = false;
     });
 }
 
@@ -25,10 +65,16 @@ var GEO_AGGREGATION_RESOLUTION = 1000;
  * And filling in the four separate data arrays
  */
 function ParseData(data) {
+    // Clear all existing data
+    tempData.clear();
+    humiData.clear();
+    partData.clear();
+    vocsData.clear();
+    
     // Repackage data into four separate arrays
-    aggregateData = {};
+    var aggregateData = {};
     for (var index in data) {
-        var key = Math.round(data[index].ycoord * GEO_AGGREGATION_RESOLUTION) + ',' 
+        var key = Math.round(data[index].ycoord * GEO_AGGREGATION_RESOLUTION) + ','
                 + Math.round(data[index].xcoord * GEO_AGGREGATION_RESOLUTION);
         if (key in aggregateData) {
             aggregateData[key].temperature += data[index].temperature;
@@ -41,7 +87,8 @@ function ParseData(data) {
             aggregateData[key].count = 1;
         }
     }
-    
+
+    // Calculate an average coordinate (only used when initializing the map)
     var averageX = 0;
     var averageY = 0;
     var countGeo = 0;
@@ -51,72 +98,94 @@ function ParseData(data) {
         humiData.push({ location: coord, weight: aggregateData[key].humidity    / aggregateData[key].count });
         partData.push({ location: coord, weight: aggregateData[key].particulate / aggregateData[key].count });
         vocsData.push({ location: coord, weight: aggregateData[key].gas         / aggregateData[key].count });
-        
+
         averageX += aggregateData[key].xcoord;
         averageY += aggregateData[key].ycoord;
         countGeo++;
     }
-    
+
     return new google.maps.LatLng(averageY / countGeo, averageX / countGeo);
 }
 
-var map;
-var tempHeatmap;
-var humiHeatmap;
-var partHeatmap;
-var vocsHeatmap;
-
-/**
- * Initializes the heatmap with data held in variables declared above
- */
-function InitializeHeatmap(center) {
-    var mapOptions = {
-        zoom: 15, // Level at which some landmarks and streets are visible
-        center: center, 
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-
-    // Initialize Google Maps layer
-    map = new google.maps.Map(document.getElementById('map-canvas'),
-        mapOptions);
-
-    tempHeatmap = new google.maps.visualization.HeatmapLayer({
-        data: new google.maps.MVCArray(tempData)
-    });
-    humiHeatmap = new google.maps.visualization.HeatmapLayer({
-        data: new google.maps.MVCArray(humiData)
-    });
-    partHeatmap = new google.maps.visualization.HeatmapLayer({
-        data: new google.maps.MVCArray(partData)
-    });
-    vocsHeatmap = new google.maps.visualization.HeatmapLayer({
-        data: new google.maps.MVCArray(vocsData)
-    });
-}
-
 /***** Heatmap button handlers *****/
+
+function TimeToQueryBody(time) {
+    return time.getFullYear() 
+        + '-' + (time.getMonth() + 1)
+        + '-' + time.getDate() 
+        + ' ' + time.getHours()
+        + ':' + time.getMinutes()
+        + ':' + time.getSeconds();
+}
+$('#heat_time_day').click(function () {
+    if (isWorking || $(this).hasClass('active')) return;
+
+    var time = new Date();
+    time.setDate(time.getDate() - 1);
+    FetchData('after=' + TimeToQueryBody(time), function() {});
+    
+    $(this).addClass('active');
+    $('#heat_time_week, #heat_time_all').removeClass('active');
+});
+$('#heat_time_week').click(function () {
+    if (isWorking || $(this).hasClass('active')) return;
+
+    var time = new Date();
+    time.setDate(time.getDate() - 7);
+    FetchData('after=' + TimeToQueryBody(time), function() {});
+
+    $(this).addClass('active');
+    $('#heat_time_day, #heat_time_all').removeClass('active');
+});
+$('#heat_time_all').click(function () {
+    if (isWorking || $(this).hasClass('active')) return;
+
+    var time = new Date();
+    time.setDate(time.getDate() - 1);
+    FetchData('', function() {});
+
+    $(this).addClass('active');
+    $('#heat_time_day, #heat_time_week').removeClass('active');
+});
+
+$('#heat_type_PM').click(togglePartMap);
+$('#heat_type_VOC').click(toggleVocsMap);
+$('#heat_type_temp').click(toggleTempMap);
+$('#heat_type_humi').click(toggleHumiMap);
 
 function toggleTempMap() {
     tempHeatmap.setMap(map);
     humiHeatmap.setMap(null);
     partHeatmap.setMap(null);
     vocsHeatmap.setMap(null);
+    
+    $('#heat_type_temp').addClass('active');
+    $('#heat_type_PM, #heat_type_VOC, #heat_type_humi').removeClass('active');
 }
 function toggleHumiMap() {
     tempHeatmap.setMap(null);
     humiHeatmap.setMap(map);
     partHeatmap.setMap(null);
     vocsHeatmap.setMap(null);
+    
+    $('#heat_type_humi').addClass('active');
+    $('#heat_type_PM, #heat_type_VOC, #heat_type_temp').removeClass('active');
 }
 function togglePartMap() {
     tempHeatmap.setMap(null);
     humiHeatmap.setMap(null);
     partHeatmap.setMap(map);
     vocsHeatmap.setMap(null);
+    
+    $('#heat_type_PM').addClass('active');
+    $('#heat_type_VOC, #heat_type_temp, #heat_type_humi').removeClass('active');
 }
 function toggleVocsMap() {
     tempHeatmap.setMap(null);
     humiHeatmap.setMap(null);
     partHeatmap.setMap(null);
     vocsHeatmap.setMap(map);
+    
+    $('#heat_type_VOC').addClass('active');
+    $('#heat_type_PM, #heat_type_temp, #heat_type_humi').removeClass('active');
 }
